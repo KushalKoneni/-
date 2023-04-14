2d0e1eb8ad61b6283c9bb1cfa7bf4464891936b3
KushalKoneni <konenikushal@gmail.com>
2023-04-13 21:41:34 -0400

    filed away the dat we need

--- database/schema.sql
+++ database/schema.sql
@@ -1,30 +1,7 @@
-CREATE TABLE inventory (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    item_name VARCHAR(255) NOT NULL,
-    info VARCHAR(255) NOT NULL,
-    price DECIMAL(10,2) NOT NULL,
-    stock INTEGER NOT NULL,
-    image_url VARCHAR(255) NOT NULL,
-    category VARCHAR(255) NOT NULL
+CREATE TABLE catalogue (
+    city_1 VARCHAR(50) NOT NULL,
+    city_2 VARCHAR(50) NOT NULL,
+    nautical_miles INTEGER NOT NULL,
+    time_minutes INTEGER NOT NULL,
+    PRIMARY KEY (city_1, city_2)
 );
-
-CREATE TABLE users (
-    username VARCHAR(255) PRIMARY KEY NOT NULL,
-    password_hash VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE sales (
-    sale_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    transaction_id VARCHAR(255) NOT NULL,
-    username VARCHAR(255) NOT NULL,
-    item_id INTEGER NOT NULL,
-    quantity INTEGER NOT NULL,
-    sale_date DATETIME NOT NULL,
-    cost DECIMAL(10,2) NOT NULL,
-    FOREIGN KEY (username) REFERENCES users(username),
-    FOREIGN KEY (item_id) REFERENCES inventory(id)
-);
-
